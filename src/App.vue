e6a3bf896a8cd9186c50ec9cbe03f32705db6e35
ye <qw979295812@qq.com>
2019-02-13 21:47:46 +0800

    项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+ <mt-header fixed title="固定在顶部"></mt-header>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
